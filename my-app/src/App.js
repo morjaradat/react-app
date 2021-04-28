9a07eb7da912093fd5c3448dfd8f95ed4da4b46b
Mohammad jaradat <morjaradat66@gmail.com>
2021-04-28 14:50:00 +0300

    solve lab 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './componet/header';
 import Main from './componet/main';
 import Footer from './componet/footer';
+import data from './assets/data.json';
+import SelectedBeast from '../src/componet/SelectedBeast';
+
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      data:data,
+      show : false,
+      details :{},
+    }
+  }
+  viewbeast =(view) =>{
+    this.setState({
+      show : true,
+      details : view,
+      
+    })
+    console.log(view)
+  };
+  handleClose=() =>{
+    this.setState({
+      show : false,
+      details : {},
+
+  })
+}
+
 render(){
   return ( 
   <div>
 <Header/>
-<Main/>
+<SelectedBeast   handleClose={this.handleClose} 
+ show={this.state.show} 
+  details={this.state.details} />
+
+<Main data ={this.state.data} viewbeast={this.viewbeast} />
+
 <Footer/>
 
   </div>
